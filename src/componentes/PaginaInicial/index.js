8d251004608f9abe99b661ed89b1359b180b0baf
tnmatheus <mth7768@gmai.com>
2024-06-03 15:18:44 -0300

    Finalização do projeto

--- src/componentes/PaginaInicial/index.js
+++ src/componentes/PaginaInicial/index.js
@@ -1,18 +1,27 @@
-import React, { useState } from "react";
-import "./estilo.css";
+import React, { useState } from 'react';
+import './estilo.css';
 
 export default function PaginaInicial() {
-  const [numeroAleatorio, setNumeroAleatorio] = useState(1);
+  const [ numeroAleatorio, setNumeroAleatorio ] = useState(1);
 
-  return (
+  function gerarNumero() {
+    const novoNumero = Math.floor(Math.random() * (100-1) + 1);
+    setNumeroAleatorio(novoNumero);
+  }
+  
+  return(
     <div className="conteudo-centralizado">
       <h3>Número aleatório:</h3>
       <h1>{numeroAleatorio}</h1>
 
-      <div className="area-botao">
-        <label>Click no botão abaixo para gerar um número aleatório:</label>
-
-        <button>Gerar número</button>
+      <div className='area-botao'>
+        <label>
+          Click no botão abaixo para gerar um número aleatório:
+        </label>
+        
+        <button onClick={gerarNumero}>
+          Gerar número
+        </button>
       </div>
     </div>
   );
